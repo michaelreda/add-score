0d1845e6827bd887e26f4eaf45eb22cefd1df97b
Michael Reda <michaelreda96@gmail.com>
2021-02-11 22:48:03 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {
   VERSION,
   ViewChild
 } from "@angular/core";
-import $ from "jquery";
 
 @Component({
   selector: "my-app",
@@ -15,45 +14,17 @@ import $ from "jquery";
 })
 export class AppComponent implements OnInit {
   name = "Angular " + VERSION.major;
-  @ViewChild("iframe") iframe: ElementRef;
-  url =
-    "https://api.allorigins.win/get?url=" +
-    encodeURIComponent("https://keepthescore.co/board/vpawwrumadr/") +
-    "&callback=?";
-
+ 
   constructor(private http: HttpClient) {
-    console.log(this.url);
+  }
+  ngOnInit() {
+   
   }
 
-  isIFrame = (input: HTMLElement | null): input is HTMLIFrameElement =>
-    input !== null && input.tagName === "IFRAME";
-  ngOnInit() {
-    // var iframe = document.getElementById("iframe");
-    // var div = document.getElementById("PlaceToPutTable");
-    // if (iframe instanceof HTMLIFrameElement ) {
-    //   console.log(iframe.contentWindow.document.getElementById(
-    //     "full-monty"
-    //   ));
-    //   div.innerHTML = iframe.contentWindow.document.getElementById(
-    //     "full-monty"
-    //   ).innerHTML;
-    // }
-    // this.http.get(this.url, { responseType: "text" }).subscribe(res => {
-    //   res = res.substring(32);
-    //   res = res.slice(0, -2);
-    //   res = JSON.parse(res);
-    //   let html = res["contents"];
-    //   this.http
-    //     .post(
-    //       "https://cors-anywhere.herokuapp.com/" +
-    //         encodeURIComponent("https://www.html2json.com/api/v1"),
-    //       html
-    //     )
-    //     .subscribe(htmlJson => {
-    //       console.log(htmlJson);
-    //     });
-    //   // console.log(res);
-    //   // $("iframe").html(res["contents"]);
-    // });
+  urlChanged() {
+    var iframe = document.getElementsByTagName("iframe")[0];
+    if (iframe.getAttribute("src").indexOf("round_add") == -1) {
+      window.location.href = "https://michaelreda.github.io/leaderboard/";
+    }
   }
 }
